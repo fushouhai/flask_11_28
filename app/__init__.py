ebec38b7bbd12e407396966a7a7cd4c05eceee45
fushouhai <1821938264@qq.com>
2016-11-30 13:00:00 +0800

    again

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,9 @@ login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
+    if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
+        from flask_sslify import SSLify
+        sslify = SSLify(app)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
@@ -43,7 +46,4 @@ def create_app(config_name):
     from .api_1_0 import api as api_1_0_blueprint
     app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
 
-    if not app.config['SSL_DISABLE']:
-        from flask_sslify import SSLify
-        sslify = SSLify(app) 
     return app
