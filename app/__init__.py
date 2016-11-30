87a54bda0aa6fd733275e26a61eed61162a87afa
fushouhai <1821938264@qq.com>
2016-11-30 12:02:59 +0800

    fix sslify

--- app/__init__.py
+++ app/__init__.py
@@ -30,9 +30,9 @@ def create_app(config_name):
     login_manager.init_app(app)
     pagedown.init_app(app)
     
-    if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
-        from flask_sslify import SSLify
-        sslify = SSLify(app)
+    #if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
+    #    from flask_sslify import SSLify
+    #    sslify = SSLify(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
@@ -42,9 +42,8 @@ def create_app(config_name):
     
     from .api_1_0 import api as api_1_0_blueprint
     app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
-    
-    #if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
-    #    from flask_sslify import SSLify
-    #    sslify = SSLify(app)
 
+    if not app.config['SSL_DISABLE']:
+        from flask_sslify import SSLify
+        sslify = SSLify(app) 
     return app
