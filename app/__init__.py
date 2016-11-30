43d8581867c7a4cfbaf30833d321e44a47427439
fushouhai <1821938264@qq.com>
2016-11-30 13:50:24 +0800

    ...

--- app/__init__.py
+++ app/__init__.py
@@ -21,10 +21,6 @@ login_manager.login_view = 'auth.login'
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    #if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
-    #from flask_sslify import SSLify
-    #sslify = SSLify(app)
-    
     config[config_name].init_app(app)
 
     bootstrap.init_app(app)
@@ -34,9 +30,9 @@ def create_app(config_name):
     login_manager.init_app(app)
     pagedown.init_app(app)
     
-    if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
-        from flask_sslify import SSLify
-        sslify = SSLify(app)
+    #if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
+    from flask_sslify import SSLify
+    sslify = SSLify(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
