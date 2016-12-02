1eb089f7c859e5d0ad3a6f94a0a49374d43145d6
fushouhai <1821938264@qq.com>
2016-12-02 16:49:21 +0800

    >>>

--- manage.py
+++ manage.py
@@ -25,7 +25,7 @@ manager.add_command('db', MigrateCommand)
 @manager.command
 def test(coverage=False):
     """Run the unit tests."""
-    if coverage and not os.environ.get('FLASK_COVERAGE')
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
         import sys
         os.environ['FLASK_COVERAGE'] = '1'
         os.execvp(sys.executable, [sys.executable] + sys.argv)
